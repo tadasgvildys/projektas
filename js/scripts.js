cf54a40687333bff8790f8fb34ef4d6e677c6a3d
tadas <tadas.gvildys@gmail.com>
2018-12-13 03:30:24 +0200

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,3 @@
-//Start Youtube API
-  // Load the IFrame Player API code asynchronously.
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  // Replace the 'ytplayer' element with an <iframe> and
-  // YouTube player after the API code downloads.
-  var player;
-  function onYouTubePlayerAPIReady() {  
-    player = new YT.Player('1player', {
-      height: '390',
-      width: '640',      
-      events: {
-            'onStateChange': function(event) {
-              if (event.data == YT.PlayerState.PLAYING) {
-                  myFunction();
-              }
-            }
-          }  
-    });           
-  }
+
 
